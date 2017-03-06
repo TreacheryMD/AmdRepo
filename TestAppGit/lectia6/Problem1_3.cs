9ed89e2749c55e23cd4f52f42d05801418866051
TreacheryMD <draganel_ion@yahoo.com>
2017-03-06 12:27:37 +0200

    problama13

---
+++ TestAppGit/lectia6/Problem1_3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lectia6
+{
+    class Problem1_3
+    {
+        public static int?[] Array { get; set; }
+        public Problem1_3(int?[] array)
+        {
+            Array = array;
+        }
+
+        public void Solve()
+        {
+            List<int?> result = new List<int?>();
+            
+            for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = i + 1; j <= Array.Length - 1; j++)
+                {
+                    if (Array[i] == Array[j])
+                    {
+                        Array[i] = null;
+                        break;
+                    }
+                }
+            }
+
+            foreach (var item in Array)
+            {
+                if (item != null)
+                {
+                    result.Add(item);
+                }
+            }
+
+            foreach (var item in result.ToArray())
+            {
+                Console.Write($"{item} ");
+            }
+            
+        }
+    }
+}
---
+++ TestAppGit/lectia6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lectia6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1_3 problem13 = new Problem1_3(new int?[] { 1, 1, 2,0, 2, 4, 4, 1, 1, 2, 2 });
+            problem13.Solve();
+        }
+    }
+}
