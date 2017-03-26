767d2f05156b67268088c0c217b2900b5a07ea6e
Treachery <Treachery@DESKTOP-F879NAM>
2017-03-26 22:31:33 +0300

    home commit

---
+++ TestAppGit/StrategyPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HiringStrategyContext hireEmployee = new HiringStrategyContext(new SoftwareEngineer());
+
+            Console.WriteLine("***Strategy for Software Engineer");
+            hireEmployee.DefineStrategies();
+
+            
+            hireEmployee = new HiringStrategyContext(new SeniorSoftwareEngineer());
+
+            Console.WriteLine("\n***Strategy for Senior Software Engineer");
+            hireEmployee.DefineStrategies();
+
+
+            hireEmployee = new HiringStrategyContext(new TeamLead());
+
+            Console.WriteLine("\n***Strategy for TeamLead");
+            hireEmployee.DefineStrategies();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TestAppGit/StrategyPattern/SoftwareEngineer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    class SoftwareEngineer : ICompanyStrategy
+    {
+        public string EmployeeStrategies()
+        {
+            return "Policies for Software Sngineer";
+        }
+    }
+}
